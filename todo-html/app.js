fbc9b25a763a5bdc00347685948b107d4fd6836f
Alaaldin98 <alaaalhanene@gmail.com>
2022-02-13 23:50:35 +0200

    dojs

--- todo-html/app.js
+++ todo-html/app.js
@@ -1,43 +1,42 @@
-console.log("My name is Alaaaldin");
-var x = 4;
-var y = " Hello";
-var z;
-z = "Hello everyone";
-console.log
-// alert("enter his/her name")
+// console.log("My name is Alaaaldin");
+// var x = 4;
+// var y = " Hello";
+// var z;
+// z = "Hello everyone";
 
-var name = prompt("Please enter your Name and Surname","Harry Potter");
-        var gender = prompt("Please enter your gender","male");
+
+// var names = prompt("Please enter your Name and Surname");
+//         var gender = prompt("Please enter your gender");
 
         
     
-        var age = prompt("Please enter your age",);
+//         var age = prompt("Please enter your age",);
 
-        if (age <= 0)
-        {
-            alert("Your input is wrong " );
-        }
-           
+//         if (age <= 0)
+//         {
+//             alert("Your input is wrong " );
+//         }
+          
     
-        confirm("Skip The Welcoming Message");
+//         confirm("Skip The Welcoming Message");
 
-        if (gender == 'male')
-        {
-            alert("Welcome, MR. " + name);
+//         if (gender == 'male')
+//         {
+//             alert("Welcome, MR. " + names);
 
-        }
-        else if (gender == 'female')
-        {
-            alert("Welcome, MS. " + name);
+//         }
+//         else if (gender == 'female')
+//         {
+//             alert("Welcome, MS. " + names);
 
-        } else {
-            alert("");
-        }
+//         } else {
+//             alert("");
+//         }
         
 
 
-     console.log(x+y);
-console.log(z);
+//      console.log(x+y);
+// console.log(z);
 // var promptAnswer= prompt("Taype your Name");
 // var promptAnswer= prompt("Enter his/her gender");
 // console.log(typeof(promptAnswer));
@@ -76,4 +75,76 @@ console.log(z);
 
 //     default:
 //         break;
+// *****
+// var solve1 = "yes"; 
+//         function yesono(answer) {
+//     var ques = [] ; 
+//     ques.push("")
+//     return ques;      
+//         }
+        // console.log (yesono());
+
+        function question1(q1) {
+            var question1 =prompt("Do you love Summer?");
+            return question1;
+          
+        }
+        
+        
+        function question2(q2) {
+            var question2 = prompt("Do you like Butterfly?");
+            return question2;
+        }
+
+        function question3(q3) {
+        var question3 = prompt("Do you have a car?");
+        return question3 ;
+        }
+//         function loop(params) {
+//             for (i = 0; i < ques.length; i++) {
+               
+//             }
+//         }
+// ****
+    //    var arr[];
+        // function condition(params) {
+        //     if 
+    
+        // }
+        // var userAnswers = [];
+        // var questions = ["Hi Alaa","Hi Ali","Hi Adel"];
+        // for( var i = 0; i < questions.length; i++ ) {
+              
+          
+        //     userAnswers[i] = prompt(questions[i]);
+        //     if (userAnswers[i]=="") {
+        //         userAnswers[i]= "InValid";
+        // }}
+        // console.log("userAnswers", userAnswers)
+
+        var userAnswers = [];
+        var questions = [question1,"Hi Ali","Hi Adel"];
+        
+           
+            
+            userAnswers[0] = question1();
+            
+            userAnswers[1] = question2();
+            userAnswers[2] = question3();
+            for( var i = 0; i < questions.length; i++ ) {
+            if(userAnswers[i] == "") userAnswers[i] = "invalid "
+            }
+        console.log("userAnswers", userAnswers)
+
+
+
+
+
+        // var userAnswers = [];
+        // var questions = ["Hi Alaa","Hi Ali","Hi Adel"];
+        // for( var i = 0; i < questions.length; i++ ) {
+        //     userAnswers[i] = prompt(questions[i]);
+        //     if(userAnswers[i] == "") userAnswers[i] = "invalid "
+        // }
+        // console.log("userAnswers", userAnswers)
         
